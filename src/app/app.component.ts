0892ff0af1809d90760009937607fe1ee44ae6c0
Christopher <cquirozmendivel@gmail.com>
2024-07-07 14:37:00 +0200

    Fix styles RPW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,14 @@ export class AppComponent {
     {
       imageUrl: '/assets/img/user/image-tanya.jpg',
       testimonial:
-        'I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.',
+        '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
       name: 'Tany Sinclair',
       skill: 'UX Engineer',
     },
     {
       imageUrl: '/assets/img/user/image-john.jpg',
       testimonial:
-        'If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.',
+        '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
       name: 'John Tarkpor',
       skill: 'Junior Front-end Developer',
     },
